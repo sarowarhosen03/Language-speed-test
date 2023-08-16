0b57f0f6bcb421f68a1886c2caa34367b4d89ae2
Your Name <you@example.com>
2023-08-16 15:45:04 +0600

    added java branchmark

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static long fibonacci(long n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        long n = 35; // Adjust the value as needed
+        System.out.println("Java Result: " + fibonacci(n));
+        long endTime = System.nanoTime();
+        double timeTaken = (endTime - startTime) / 1e6; // Convert to milliseconds
+        System.out.println("Time taken: " + timeTaken + " milliseconds");
+    }
+}
+
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-node node2.js && go run go2.go  && ./c && ./cpp && ./rust && python3 python.py && php php.php
+node node2.js && go run go2.go  && ./c && ./cpp && ./rust && python3 python.py && php php.php && java Main
