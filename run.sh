56f8f4114d10ba220d4b39cf61b6de0919c500a0
Your Name <you@example.com>
2023-08-18 07:58:12 +0600

    go binary excitable file added

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-node node2.js && go run go2.go  && ./c && ./cpp && ./rust && python3 python.py && php php.php && java Main
+node node2.js && ./go2  && ./c && ./cpp && ./rust && python3 python.py && php php.php && java Main
