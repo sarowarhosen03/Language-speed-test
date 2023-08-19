49773575a63a540bfc1e1d80d7a29a489641d576
Your Name <you@example.com>
2023-08-19 15:31:31 +0600

    update php.php

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         long startTime = System.nanoTime();
         long n = 35; // Adjust the value as needed
-        System.out.println("Java Result: " + fibonacci(n));
+        System.out.print("Java Result: " + fibonacci(n));
         long endTime = System.nanoTime();
         double timeTaken = (endTime - startTime) / 1e6; // Convert to milliseconds
         System.out.println("Time taken: " + timeTaken + " milliseconds");
--- php.php
+++ php.php
@@ -11,6 +11,6 @@ $n = 35; // Adjust the value as needed
 echo "PHP Result: " . fibonacci($n) . "\n";
 $end_time = microtime(true);
 $time_taken = $end_time - $start_time;
-echo "Time taken: " . number_format($time_taken, 6) . " seconds\n";
+echo "Time taken: " . number_format($time_taken, 6) . " seconds";
 ?>
 
